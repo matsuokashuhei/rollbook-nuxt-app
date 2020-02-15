067598afd9c6c02d2b3e28e3c11e700e1235c957
Shuhei Matsuoka <matsuokashuheiii@gmail.com>
2020-02-15 15:49:14 +0000

    Setup TypeScript

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,8 @@ export default {
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    '@nuxt/typescript-build'
   ],
   /*
    ** Nuxt.js modules
