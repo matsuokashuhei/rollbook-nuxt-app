a6667781bfdd2bd5ad770735504418d848606175
matsuokashuhei <matsuokashuheiii@gmail.com>
2020-04-10 00:13:24 +0900

    Update pages/users/_id.vue

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -12,6 +12,17 @@
         <a-radio value="2">Venue</a-radio>
       </a-radio-group>
     </a-form-model-item>
+    <a-form-model-item>
+      <a-button type="primary" @click="onSubmit">Update</a-button>
+    </a-form-model-item>
+    <a-alert type="success" v-if="message" showIcon>
+      <template slot="message">{{ message }}</template>
+    </a-alert>
+    <template v-if="errors">
+      <a-alert type="error" v-for="error in errors" :key="error.message" showIcon>
+        <template slot="message">{{ error.message }}</template>
+      </a-alert>
+    </template>
   </a-form-modal>
 </template>
 
@@ -22,11 +33,15 @@ export default {
       labelCol: { span: 4 },
       wrapperCol: { span: 14 },
       form: {
+        id: '',
         name: '',
         email: '',
         role: '',
         status: ''
-      }
+      },
+      loading: false,
+      message: '',
+      errors: []
     }
   },
   mounted() {
@@ -38,6 +53,28 @@ export default {
         `/api/users/${this.$route.params.id}`
       )
       this.form = response
+    },
+    async onSubmit(e) {
+      e.preventDefault()
+      this.setLoading()
+      try {
+        const response = await this.$axios.$put(`/api/users/${this.form.id}`, {
+          user: {
+            name: this.form.name
+          }
+        })
+        this.message = `${this.form.name}を更新しました`
+      } catch (e) {
+        console.error(e)
+        this.errors = e.response.data.errors
+      } finally {
+        this.loading = true
+      }
+    },
+    setLoading() {
+      this.message = ''
+      this.errors = []
+      this.loading = true
     }
   }
 }
